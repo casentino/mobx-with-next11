08bd24f1c0e0100ffd8139532629c8326e1ee324
casentino <opterk@gmail.com>
2023-04-06 05:40:29 +0900

    TodoStore 와 Todo 타입 정의

---
+++ pages/stores/TodoStore.ts
@@ -0,0 +1,28 @@
+import { makeAutoObservable } from 'mobx';
+import Todo from '../models/Todo';
+
+export default class TodoStore {
+	private _todoList: Todo[] = [];
+
+	constructor() {
+		makeAutoObservable(this);
+	}
+	addTodo(todo: Todo) {
+		this._todoList.push(todo);
+	}
+
+	removeTodo(id: number) {
+		this._todoList = this._todoList.filter((todo) => todo.id !== id);
+	}
+
+	get todoList() {
+		return this._todoList;
+	}
+	hydrate(hydrateData?: TodoStore) {
+		if (!hydrateData) return;
+
+		if (hydrateData.todoList) {
+			this._todoList = hydrateData.todoList;
+		}
+	}
+}
