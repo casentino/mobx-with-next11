bc42d2cd4f838916388042eaf7447157b17a6545
casentino <opterk@gmail.com>
2023-04-06 06:20:24 +0900

    Hydrate 해야하는 스토어들 interface 정의

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,18 @@ import type { AppContext, AppProps } from 'next/app';
 import { RootStoreProvider } from './stores/RootStoreProvider';
 import { RootStore } from './stores/RootStore';
 import { NextPage } from 'next';
+import { HydrateStoreData } from './stores/HydrationType';
 
 interface MyAppProps extends AppProps {
-	hydrationData: RootStore;
+  hydrationData: HydrateStoreData;
 }
 
 function MyApp({ Component, pageProps, hydrationData }: MyAppProps) {
-	return (
-		<RootStoreProvider hydrationData={hydrationData}>
-			<Component {...pageProps} />
-		</RootStoreProvider>
-	);
+  return (
+    <RootStoreProvider hydrationData={hydrationData}>
+      <Component {...pageProps} />
+    </RootStoreProvider>
+  );
 }
 
 export default MyApp;
