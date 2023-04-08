19002df242cc75165ac11dcb0f3761d678b05a5c
casentino <opterk@gmail.com>
2023-04-08 16:27:47 +0900

    Store deserialize

--- pages/utils/hydrationUtil.ts
+++ pages/utils/hydrationUtil.ts
@@ -1,6 +1,7 @@
 // import { HydrationStore, RootStoreMembers } from '../stores/RootStore';
-import { ObservableMap, ObservableSet, toJS } from 'mobx';
-import { HydrationStore, RootStoreMembers } from '../stores/HydrationType';
+import { observable, ObservableMap, ObservableSet, toJS } from 'mobx';
+import { HydrateStoreData, HydrationStore, RootStoreMembers } from '../stores/HydrationType';
+import { RootStore } from '../stores/RootStore';
 
 export function serializationStore<Store extends RootStoreMembers>(store: Store): HydrationStore<Store> {
 	function parser(o: Record<string, unknown>) {
@@ -13,7 +14,7 @@ export function serializationStore<Store extends RootStoreMembers>(store: Store)
 
 			if (typeof parsedValue === 'object') {
 				if (parsedValue instanceof Map || parsedValue instanceof ObservableMap) {
-					parsedValue = Object.fromEntries(Array.from(parsedValue.entries()));
+					parsedValue = Array.from(parsedValue.entries());
 				}
 				if (parsedValue instanceof Set || parsedValue instanceof ObservableSet) {
 					parsedValue = Array.from(parsedValue.values());
@@ -37,4 +38,37 @@ export function serializationStore<Store extends RootStoreMembers>(store: Store)
 		};
 	}, {});
 	return serializeStore;
+}
+
+type HydratatoinStoreKeys<Store extends RootStoreMembers> = keyof HydrationStore<Store>;
+type EntriesType<Store extends RootStoreMembers> = [HydratatoinStoreKeys<Store>, Store[HydratatoinStoreKeys<Store>]];
+
+export function deserializationStore<Store extends RootStoreMembers>(
+	serailizedStore: HydrationStore<Store>,
+	store: Store
+): HydrationStore<Store> {
+	const entreis = Object.entries<HydrationStore<Store>[HydratatoinStoreKeys<Store>]>(serailizedStore) as Array<
+		EntriesType<Store>
+	>;
+	return entreis.reduce<HydrationStore<Store>>((prev, curr) => {
+		const [key, value] = curr;
+		if (value instanceof Array) {
+			if (store[key] instanceof ObservableSet || store[key] instanceof Set) {
+				return {
+					...prev,
+					[key]: observable.set(value),
+				};
+			}
+			if (store[key] instanceof ObservableMap || store[key] instanceof Map) {
+				return {
+					...prev,
+					[key]: observable.map(value),
+				};
+			}
+		}
+		return {
+			...prev,
+			[key]: value,
+		};
+	}, {});
 }
