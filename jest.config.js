3db9657cae7f86f13cfea5395239bc97410ae68b
casentino <opterk@gmail.com>
2023-04-10 04:38:52 +0900

    [TEST]hydrationUtil / jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+	preset: 'ts-jest',
+	testEnvironment: 'node',	
+	testMatch: [ '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+	transform: {
+		'^.+\\.(ts|tsx)$': 'ts-jest',
+	},
 };
