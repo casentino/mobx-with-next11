93ad1374fb73792ad29a96cc7d707998efdda2f7
casentino <opterk@gmail.com>
2023-04-18 17:57:39 +0900

    path 설정, env 설정

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  webpack5: false,
+
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+function settingEnv(phase) {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const env = {
+    DEV_SERVER_URL: (() => { 
+      if (isDev) { 
+        return 'http://localhost:8080'
+      }
+    })()
+  }
+  return {
+    env,
+		reactStrictMode: true,
+		webpack5: false,
+	};
 }
+
+module.exports = settingEnv;
