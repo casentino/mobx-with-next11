f8c796fb14283fee5e5c28d7ff460d0d662c9b30
casentino <opterk@gmail.com>
2023-04-25 17:26:44 +0900

    env config

---
+++ src/config/config.ts
@@ -0,0 +1,3 @@
+export const DEV_SERVER_URL = process.env.DEV_SERVER_URL || ('' as const);
+export const COOKIE_SESSION_ID = 'SESSION_ID' as const;
+export const COOKIE_REFRESH_SESSION = 'SESSION_REFRESH' as const;
