cc8e724dc2055757f9fa3d4938d48e1e55015e03
casentino <opterk@gmail.com>
2023-05-02 19:16:05 +0900

    불필요코드 제거, type 수정

--- src/utils/hydrationUtil.ts
+++ src/utils/hydrationUtil.ts
@@ -1,13 +1,17 @@
 // import { HydrationStore, RootStoreMembers } from '../stores/RootStore';
 import { observable, ObservableMap, ObservableSet, toJS } from 'mobx';
-import { HydrationStore, IHydrationStore } from '../stores/HydrationType';
+import { HydrationStore, IHydrationStore, Keys } from '../stores/HydrationType';
+
+function isNullable(value: unknown) {
+	if (value === undefined || value === null || Number.isNaN(value)) {
+		return true;
+	}
+	return false;
+}
 
 export function serializationStore<Store extends IHydrationStore>(store: Store): HydrationStore<Store> {
-	function parser(o: Record<string, unknown>) {
-		if (o instanceof Function) {
-			return o;
-		}
-		return Object.entries(store).reduce((prev, curr) => {
+	function parser(o: Record<string, any>) {
+		return Object.entries(o).reduce((prev, curr) => {
 			const [key, value] = curr;
 			let parsedValue = value;
 
@@ -19,37 +23,38 @@ export function serializationStore<Store extends IHydrationStore>(store: Store):
 					parsedValue = Array.from(parsedValue.values());
 				}
 			}
-
+			if (value instanceof Function || isNullable(value)) {
+				return prev;
+			}
 			return {
 				...prev,
-				[key]: parsedValue,
+				[key.replace('_', '')]: parsedValue,
 			};
 		}, {});
 	}
-
-	const strStore = JSON.stringify(parser(toJS(store) as any));
-
-	const serializeStore = Object.entries(JSON.parse(strStore)).reduce((prev, curr) => {
-		const [key, value] = curr;
-		return {
-			...prev,
-			[key.replace('_', '')]: value,
-		};
-	}, {});
-	return serializeStore;
+	return parser(toJS(store));
 }
 
 type HydratatoinStoreKeys<Store extends IHydrationStore> = keyof HydrationStore<Store>;
-type EntriesType<Store extends IHydrationStore> = [HydratatoinStoreKeys<Store>, Store[HydratatoinStoreKeys<Store>]];
-
+type EntriesType<Store extends IHydrationStore> = [keyof Store, HydrationStore<Store>[HydratatoinStoreKeys<Store>]];
+/**
+ * hydrate해줄 스토어를 첫번째 타입 매개변수로 정해주고,
+ * Hydration된 스토어를 두번째 타입 매개변수로 정해줍니다.
+ * 같은 명칭의 스토어 property를 HydrationStore 타입이 아닌
+ * 원래 Store의 property 타입으로 바꿔줍니다.
+ */
+type DesrializeStore<Store extends IHydrationStore, HStore extends HydrationStore> = Partial<{
+	[K in Keys<HStore>]: K extends keyof Store ? Store[K] : HStore[K];
+}>;
 export function deserializationStore<Store extends IHydrationStore>(
 	store: Store,
 	serailizedStore: HydrationStore<Store>
-): HydrationStore<Store> {
+): DesrializeStore<Store, HydrationStore<Store>> {
 	const entreis = Object.entries<HydrationStore<Store>[HydratatoinStoreKeys<Store>]>(serailizedStore) as Array<
 		EntriesType<Store>
 	>;
-	return entreis.reduce<HydrationStore<Store>>((prev, curr) => {
+
+	return entreis.reduce<DesrializeStore<Store, HydrationStore<Store>>>((prev, curr) => {
 		const [key, value] = curr;
 		if (value instanceof Array) {
 			if (store[key] instanceof ObservableSet || store[key] instanceof Set) {
